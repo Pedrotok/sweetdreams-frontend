839b0f0dbc3a82cf7a295e37481ed492f0ba04ba
daniangelos <daniella@novatics.com.br>
2020-09-29 11:16:32 -0300

    Responsive about

--- components/home/about.js
+++ components/home/about.js
@@ -4,7 +4,7 @@ function About() {
       <h1 className="mb-4">This is your space</h1>
       <h3 className="text-opacity-75">Talk about your business, your products, yourself</h3>
       <div className="my-12 h-2px w-12 bg-black"></div>
-      <div className="flex space-x-24">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
         <p className="text-opacity-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
         <p className="text-opacity-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
       </div>
