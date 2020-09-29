65aa608a0c817df427bb13d76c49195b4f9f2550
daniangelos <daniella@novatics.com.br>
2020-09-29 18:19:02 -0300

    Add Featured Products section on home page

---
+++ components/home/featuredProducts.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+function FeaturedProducts() {
+  return (
+    <div className="m-32">
+      <h2 className="text-center">Featured Products</h2>
+      <h3 className="mt-2 text-center text-opacity-75">Check out our popular products</h3>
+      <div className="flex flex-wrap my-8 justify-center">
+        <div className="m-4 w-96 h-128 border-2 border-black"></div>
+        <div className="m-4 w-96 h-128 border-2 border-black"></div>
+        <div className="m-4 w-96 h-128 border-2 border-black"></div>
+      </div>
+      <Link href="/products">
+        <p className="text-center text-indigo text-s tracking-wider opacity-75 font-medium hover:text-darkblue hover:underline cursor-pointer">View all products</p>
+      </Link>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Carousel from 'components/home/carousel';
 import About from 'components/home/about';
+import FeaturedProducts from 'components/home/featuredProducts';
 
 export default function Index() {
   return (
-    <>
+    <div className="">
       <Carousel />
+      <FeaturedProducts />
       <About />
-    </>
+    </div>
   );
 }
