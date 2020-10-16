59d352fa309a04acae53c4144431aa36f5a2d848
Daniella Angelos <daniaangelos@gmail.com>
2020-10-16 20:28:56 -0300

    Center title on products page

--- components/productItem.js
+++ components/productItem.js
@@ -1,15 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function ProductItem({ product }) {
   return (
     <Link href="/products/[id]" as={`/products/${product._id}`}>
-      <div className="max-w-xxs mx-auto mb-5 cursor-pointer">
-        <img className="w-full" src={product.imageUrl} alt={product.description} />
+      <div className="max-w-xxs m-4 cursor-pointer">
+        <img
+          className="w-full"
+          src={product.imageUrl}
+          alt={product.description}
+        />
         <div className="text-sm text-center">
           <p>{product.name.toUpperCase()}</p>
-          <p className="font-bold">
-            R$ {product.price.toFixed(2)}
-          </p>
+          <p className="font-bold">R$ {product.price.toFixed(2)}</p>
         </div>
       </div>
     </Link>
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,32 +1,29 @@
-import { getProducts } from 'lib/products'
-import ProductItem from 'components/productItem'
+import { getProducts } from 'lib/products';
+import ProductItem from 'components/productItem';
 
 export default function Products(props) {
   const products = props.products;
   return (
     <div>
-      <div className="hero is-primary">
-        <div className="hero-body container">
-          <h4 className="title">Our Products</h4>
+      <div className="m-12 hero is-primary">
+        <div className="m-auto hero-body container">
+          <h1 className="title text-center">Our Products</h1>
         </div>
       </div>
       <br />
-      <div className="mx-auto">
-        <div className="flex flex-wrap">
+      <div className="mx-auto mb-16">
+        <div className="flex flex-wrap justify-around">
           {products && products.length ? (
             products.map((product, index) => (
-              <ProductItem
-                product={product}
-                key={index}
-              />
+              <ProductItem product={product} key={index} />
             ))
           ) : (
-              <div className="column">
-                <span className="title has-text-grey-light">
-                  No product found!
+            <div className="column">
+              <span className="title has-text-grey-light">
+                No product found!
               </span>
-              </div>
-            )}
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -37,7 +34,7 @@ export async function getStaticProps() {
   const products = await getProducts(0);
   return {
     props: {
-      products
-    }
+      products,
+    },
   };
 }
