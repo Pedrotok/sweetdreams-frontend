4b9c44487ea03fdf9514b1059d0a0400d56c615b
Daniella Angelos <daniaangelos@gmail.com>
2021-09-07 16:14:37 -0300

    Call register endpoint on sign up

--- components/signUp.js
+++ components/signUp.js
@@ -1,13 +1,26 @@
 import { useFormik } from 'formik';
-import { authenticate } from 'lib/user';
+import { register } from 'lib/user';
 
 const validate = values => {
   const errors = {};
+  const requiredField = 'Campo obrigatório';
+  if (!values.name) {
+    errors.name = requiredField;
+  }
   if (!values.email) {
-    errors.email = 'Required';
+    errors.email = requiredField;
   }
   else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
+    errors.email = 'Endereço de email inválido';
+  }
+  if (!values.password) {
+    errors.password = requiredField
+  }
+  else if(values.password.length < 8) {
+    errors.password = 'Senha muito curta'
+  }
+  else if(values.password != values.confirmPassword) {
+    errors.password = 'Senhas não correspondem'
   }
 
   return errors;
@@ -23,7 +36,7 @@ export default function SignUp() {
     },
     validate,
     onSubmit: values => {
-      authenticate(values.email, values.password);
+      register(values.email, values.password).then(res => console.log(res));
     },
   });
 
@@ -42,6 +55,9 @@ export default function SignUp() {
             onBlur={formik.handleBlur}
             value={formik.values.name} />
         </div>
+        {formik.touched.name && formik.errors.name ? (
+          <div className="error-message">{formik.errors.name}</div>
+        ) : null}
         <div className='py-2'>
           <label className="block" htmlFor="email">E-mail:</label>
           <input className="input-field"
@@ -54,7 +70,7 @@ export default function SignUp() {
             value={formik.values.email} />
         </div>
         {formik.touched.email && formik.errors.email ? (
-          <div>{formik.errors.email}</div>
+          <div className="error-message">{formik.errors.email}</div>
         ) : null}
         <div className="py-2">
           <label className="block" htmlFor="password">Password:</label>
@@ -67,6 +83,9 @@ export default function SignUp() {
             onBlur={formik.handleBlur}
             value={formik.values.password} />
         </div>
+        {formik.touched.password && formik.errors.password ? (
+          <div className="error-message">{formik.errors.password}</div>
+        ) : null}
         <div className="py-2">
           <label className="block" htmlFor="confirmPassword">Confirm Password:</label>
           <input className="input-field"
@@ -78,8 +97,8 @@ export default function SignUp() {
             onBlur={formik.handleBlur}
             value={formik.values.confirmPassword} />
         </div>
-        <div class="flex items-start space-x-5 py-2">
-          <button class="btn bg-gray-500 hover:bg-gray-700" type="submit">
+        <div className="flex items-start space-x-5 py-2">
+          <button className="btn bg-gray-500 hover:bg-gray-700" type="submit">
             Sign Up
           </button>
         </div>
